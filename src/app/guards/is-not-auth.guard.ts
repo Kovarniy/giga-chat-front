9b895e2dcbc511dae1ba3f534fee47b27970ab9b
Evgeniy Schemelinin <Kovarniy@users.noreply.github.com>
2022-05-29 23:03:04 +0800

    редирект на чаты, если юзер не авторизован

--- src/app/guards/is-not-auth.guard.ts
+++ src/app/guards/is-not-auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from "../services/auth.service";
 
@@ -8,7 +8,8 @@ import {AuthService} from "../services/auth.service";
 })
 export class IsNotAuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private router: Router) {
   }
 
   canActivate(
@@ -16,6 +17,12 @@ export class IsNotAuthGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
     const isNotAuth = !this.authService.isAuth();
+    console.log(route.url);
+
+    if (!isNotAuth && route.url.length === 1 && route.url[0].path === 'authorization') {
+      this.router.navigate(['/chat-application']);
+    }
+
     return isNotAuth;
   }
 
